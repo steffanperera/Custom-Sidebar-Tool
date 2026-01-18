a2866b6ff46a4b0ccfd756a916bf887351e1818e
steffanperera <steffanperera1997@gmail.com>
2026-01-18 14:45:04 +1100

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const provider = new SimpleViewProvider();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "custom-sidebar-tool" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('custom-sidebar-tool.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from custom-sidebar-tool!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.window.registerTreeDataProvider(
+      "mySidebarView",
+      provider
+    )
+  );
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class SimpleViewProvider implements vscode.TreeDataProvider<string> {
+  getTreeItem(element: string): vscode.TreeItem {
+    return new vscode.TreeItem(element);
+  }
+
+  getChildren(): string[] {
+    return ["Hello VS Code 👋"];
+  }
+}
